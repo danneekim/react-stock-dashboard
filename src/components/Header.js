72da6bf4b5afa9338b530a852978ed773e21b05b
Dannee Kim <dannee.kim.dev@gmail.com>
2022-12-24 11:30:13 -0500

    minor header changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,12 +3,10 @@ import Search from "./Search";
 
 const Header = ({ name }) => {
   return (
-    <>
-      <div>
-        <h1 className="text-5xl">{name}</h1>
-        <Search />
-      </div>
-    </>
+    <div>
+      <h1 className="text-5xl">{name}</h1>
+      <Search />
+    </div>
   );
 };
 
