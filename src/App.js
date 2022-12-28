492cbdeb754ea01113d1ad946ac84ff72550d271
Dannee Kim <dannee.kim.dev@gmail.com>
2022-12-28 14:58:40 -0500

    create global context/state for stock symbol

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import { useState } from "react";
 import "./App.css";
 import Dashboard from "./components/Dashboard";
+import StockContext from "./context/StockContext";
 import ThemeContext from "./context/ThemeContext";
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
+  const [stockSymbol, setStockSymbol] = useState("AAPL");
 
   return (
     <ThemeContext.Provider value={{ darkMode, setDarkMode }}>
-      <Dashboard></Dashboard>;
+      <StockContext.Provider value={{ stockSymbol, setStockSymbol }}>
+        <Dashboard></Dashboard>;
+      </StockContext.Provider>
     </ThemeContext.Provider>
   );
 }
