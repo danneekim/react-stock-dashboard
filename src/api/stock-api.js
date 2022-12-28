ca853989c76e5ce8d0fa296492e8b5b1575959ac
Dannee Kim <dannee.kim.dev@gmail.com>
2022-12-28 15:34:47 -0500

    create fetch api for stock details

--- src/api/stock-api.js
+++ src/api/stock-api.js
@@ -6,7 +6,20 @@ export const searchStockSymbols = async (query) => {
   const response = await fetch(url);
 
   if (!response.ok) {
-    const message = `An error has occured: ${response.status}`;
+    const message = `An error has occurred: ${response.status}`;
+    throw new Error(message);
+  }
+
+  return await response.json();
+};
+
+export const fetchStockDetails = async (stockSymbol) => {
+  // https://finnhub.io/docs/api/company-profile2
+  const url = `${basePath}/stock/profile2?symbol=${stockSymbol}&token=${process.env.REACT_APP_API_KEY}`;
+  const response = await fetch(url);
+
+  if (!response.ok) {
+    const message = `An error has occurred: ${response.status}`;
     throw new Error(message);
   }
 
